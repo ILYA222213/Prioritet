d19dc80304348504967e07bd5c3130fe086e91dd
Сом <117897751+ILYA222213@users.noreply.github.com>
2023-09-21 08:43:56 +0300

    Add files via upload

---
+++ Приоритет/Приоритет/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args) //Основной метод программы
+    {
+        Console.WriteLine("Задание 3"); 
+        AllInfoProcess(); //Вызываем метод, который будет выводить информацию о процессах
+    }
+    static void AllInfoProcess() //Метод, который выводит информацию о всех текущих процессах
+    {
+        var myProcess = from proc in Process.GetProcesses(".") //Получаем все текущие процессы и сортируем их по приоритету
+                        orderby proc.BasePriority
+                        select proc;
+        Console.WriteLine("\n*** Текущие процессы ***\n"); //Выводим заголовок для списка процессов
+        foreach (var p in myProcess) //Выводим информацию о каждрм процессе
+            Console.WriteLine("-> PID: {0}\tName: {1}", p.Id, p.ProcessName);
+
+    }
+}
